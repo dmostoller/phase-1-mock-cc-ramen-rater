96a846932b633361834a105b5dab906f8bc82ce8
David Mostoller <dmostoller@gmail.com>
2023-12-19 12:56:58 -0500

    3rd deliverable complete but not second yet

--- src/index.js
+++ src/index.js
@@ -10,11 +10,8 @@ fetch("http://localhost:3000/ramens")
 
 function renderRamen(ramenArray) {
     //console.log(ramenArray)
-
     //get the div tag
     const menuDiv = document.querySelector("#ramen-menu")
-
-
     //loop through json array objects and add to DOM with forEach loop
     ramenArray.forEach((ramenObj) => {
         //console.log(ramenObj)
@@ -22,14 +19,61 @@ function renderRamen(ramenArray) {
         const ramenImg = document.createElement('img')
         let ramenImgUrl = ramenObj.image
         ramenImg.src = ramenImgUrl
-
+        let ramenImgName = ramenObj.name
+        ramenImg.name = ramenImgName
         //append to DOM
-        menuDiv.appendChild(ramenImg)
-        
+        menuDiv.appendChild(ramenImg)        
     });
-
-
 }
 
+
+//create function to load ramen to main image after image being clicked on
+//eventlistener for onclick
+
+
+//function
+
+//figure out which was clicked
+    //const imageClicked = document.querySelector(``)
+    //imageClicked.addEventListener('click', loadRamenDetail)
+    //function loadRamenDetail(e) {
+//fetch the individual object for the image that was clicked
+        //console.log(imageClicked)}
+
+//get div for detail
+    //const detailDiv = document.querySelector("#ramen-detail")
+
+
+
+//add new RAMEN from FORM SUBMIT - POST
+
+// get form
+
+const form = document.querySelector("form")
+form.addEventListener('submit', (e) => addNewRamen(e))
+
+function addNewRamen(e) {
+    e.preventDefault()
+    //console.log(e)
+    //set new obj based on form info
+    let newRamenObj = {
+        name : e.target.name.value,
+        restaurant : e.target.restaurant.value,
+        rating : e.target.rating.value,
+        comment : e.target.comment.value,
+        image : e.target.image.value,
+    }
+    // make post request
+    fetch('http://localhost:3000/ramens', {
+        method : 'POST',
+        headers : {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(newRamenObj)
+    })    
+        .then((resp) => resp.json())
+        .then((data) => renderRamen([data]))
+}
 
 
